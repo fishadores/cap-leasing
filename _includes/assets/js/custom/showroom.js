5fdda6d07ba12354bb9f364c527de6f5da82715c
Taus Ansvig Sø <tas@Tauss-MacBook-Pro.local>
2015-12-08 22:27:09 +0100

    changes

--- _includes/assets/js/custom/showroom.js
+++ _includes/assets/js/custom/showroom.js
@@ -1,6 +1,20 @@
 $(function(){
 	$('.car-container, .priseks').click(function(event){
 		event.preventDefault();
+		var objModel=$('#modal-car-1').find('.modal-content');
+		$.get($(this).data('url'), function(data){
+			objModel.html(data);
+			$("#cl-slider-modal").owlCarousel({
+				navigation : true, // Show next and prev buttons
+				slideSpeed : 300,
+				paginationSpeed : 400,
+				singleItem:true,
+				navigationText: ["<span><i class='ion-chevron-left'></span>", "<span><i class='ion-chevron-right'></span>"]
+			});
+			$('#modal-car-1').modal('toggle');
+		});
+		
+		/*
 		$('#modal-'+$(this).data("id")).modal('toggle');
 		
 		$("#cl-slider-modal").owlCarousel({
@@ -10,6 +24,6 @@ $(function(){
 			singleItem:true,
 			navigationText: ["<span><i class='ion-chevron-left'></span>", "<span><i class='ion-chevron-right'></span>"]
 		});
-		
+		*/
 	})
 })
